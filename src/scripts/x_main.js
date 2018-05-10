64a17220d943e9e38d120119e89b837d2e3feb33
ppopeskul <pasha.9191@mail.ru>
2018-05-10 09:51:07 +0300

    Test task

---
+++ src/scripts/x_main.js
@@ -0,0 +1,32 @@
+$(function () {
+    $('.selectpicker').selectpicker();
+});
+
+$(document).ready(function() {
+    $('.btn__loading').click(function () {
+        $(this).toggleClass('btn__loading--active')
+    });
+});
+
+$(document).ready(function() {
+    $('#right-menu').sidr({
+        name: 'sidr-right',
+        side: 'right',
+        source: '#header-nav',
+        onOpen: function() {
+            $('.mobile-menu').addClass('active');
+        },
+        onClose: function() {
+            $('.mobile-menu').removeClass('active');
+        }
+    });
+});
+
+$(document).mouseup(function (e){
+    var container = $("#sidr-right");
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0) {
+        $.sidr('close', 'sidr-right');
+    }
+});
